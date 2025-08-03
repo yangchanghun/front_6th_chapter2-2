34ae33e7c74fe90ef8ef51d39ce464d89b56e9aa
yangchanghun <gory4848@naver.com>
2025-08-03 20:13:59 +0900

    기본 상품,기본 쿠폰 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "src") },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
