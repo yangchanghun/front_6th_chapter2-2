4d709c17db3606c4bde1c169ea0b07593cf22a0c
yangchanghun <gory4848@naver.com>
2025-08-03 21:29:09 +0900

    AdminPage분리; ;;;;

---
+++ src/basic/shared/libs/price.ts
@@ -0,0 +1,14 @@
+// const formatPrice = (price: number, productId?: string): string => {
+//   if (productId) {
+//     const product = products.find((p) => p.id === productId);
+//     if (product && getRemainingStock(product) <= 0) {
+//       return "SOLD OUT";
+//     }
+//   }
+
+//   if (isAdmin) {
+//     return `${price.toLocaleString()}원`;
+//   }
+
+//   return `₩${price.toLocaleString()}`;
+// };
