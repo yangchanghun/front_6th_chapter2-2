cfd361231c3635ae004b39ae7b2f5a814636ad27
yangchanghun <gory4848@naver.com>
2025-08-08 11:57:24 +0900

    배포.,,

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: './',
+    build: {
+      rollupOptions: {
+        input: {
+          basic: './index.basic.html',
+          advanced: './index.advanced.html',
+          origin: './index.origin.html',
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
@@ -12,5 +22,5 @@ export default mergeConfig(
       environment: 'jsdom',
       setupFiles: './src/setupTests.ts',
     },
-  })
+  }),
 );
